c1ef4ff1b0ba20f5a6f1dc762a050b6e5b8c7a94
Christian Nirschl <christiannirschl6@gmail.com>
2021-10-25 23:11:05 -0700

    fixed display errors

--- main/server.js
+++ main/server.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-require('console.table');
+ require('console.table');
+
 // const PORT = process.env.PORT || 3009;
 
 // app.use(express.urlencoded({extended:true}))
@@ -25,29 +26,57 @@ const db = mysql.createConnection(
               type: 'list',
               name: 'starter',
               message: 'What would you like to do?',
-              choices: ['view all departments', 'view all roles', 'view all employees','add a department', 'add a role', 'add an employee', 'update an employee role']
+              choices: ['view all departments', 'view all roles', 'view all employees','add a department', 'add a role', 'add an employee', 'update an employee role', 'Done']
           }
       ]).then(data =>{
-          if(data.choice === 'view all departments'){
+          if(data.starter === 'view all departments'){
               db.query('SELECT * FROM department', (err, data) =>{
               if (err){
                   throw err;
               } else{
-                  viewDepartments();
-                  console.table("All departments", data);
+                  console.table("All department", data);
+                  start();
 
               }
             });
-          } else if (data.choice === 'view all roles'){
-            db.query('SELECT * FROM roles', (err, data) =>{
+          } else if (data.starter === 'view all roles'){
+            db.query('SELECT * FROM role', (err, data) =>{
                 if (err){
                     throw err;
                 } else {
                     console.table("All Roles", data)
+                    start();
                 }
 
             });
-          }
+          }else if (data.starter === 'view all employees'){
+            db.query('SELECT * FROM employee', (err, data) =>{
+                if (err){
+                    throw err;
+                } else {
+                    console.table("All Roles", data)
+                    start();
+                }
+
+            });
+        } else if (data.starter === 'add a department'){
+            inquirer.prompt([
+                {
+                name: "name",
+                type: "iput",
+                message: "New department?"
+                },
+            ]).then(({name}) =>{
+                db.query('INSERT INTO department VALUES (?)'), name, (err,data) =>{
+                    if(err){
+                        throw err;
+                    } else {
+                        console.table("All Departments", data);
+                        start();
+                    };
+                };
+            });
+        }
       })
 
 
